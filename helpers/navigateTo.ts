3cecb1d7dc0a6541175a3fcf8e7e7f117f0c4ff4
Juan Falcone <35174379+FalcFalcFalc@users.noreply.github.com>
2026-05-13 00:49:18 -0300

    trying to speed up setup times

--- helpers/navigateTo.ts
+++ helpers/navigateTo.ts
@@ -1,6 +1,6 @@
 import { expect, Page } from "playwright/test";
 
 export async function navigateTo(page: Page, path: string) {
-    await page.goto(`${process.env.BASE_URL}${path}`);
-    expect(page.url()).toContain(`${process.env.BASE_URL}${path}`);
+    await page.goto(`${path}`);
+    expect(page.url()).toContain(`${path}`);
 }
